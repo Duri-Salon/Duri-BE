e05ade78d4aea5f5a96ab87ef11690e6e54ba131
taeyeon0319 <xodus1349@likelion.org>
2024-11-22 16:11:02 +0900

    feat: 공통응답코드(DURI-307)

---
+++ app/src/main/java/kr/com/duri/common/exception/IllegalParameterException.java
@@ -0,0 +1,8 @@
+package kr.com.duri.common.exception;
+
+public class IllegalParameterException extends RuntimeException {
+    public IllegalParameterException(String message, Object... args) {
+        super();
+    }
+}
+
