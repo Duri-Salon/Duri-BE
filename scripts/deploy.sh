7e5ce49597c65b6ab96807d55d87fc468f19b263
delight-es <skfmf7878@gmail.com>
2024-11-25 15:44:44 +0900

    fix : EC2 자동배포 app 모듈 하위 jar 파일로 변경 (DURI-311)

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -6,16 +6,16 @@ APP_NAME=duri
 REPOSITORY=/home/ubuntu/duri
 cd $REPOSITORY
 
-# 압축 해제
+# 1. 압축 해제
 echo "압축 해제 시작..."
 unzip -o $REPOSITORY/duri-aws-build.zip -d $REPOSITORY/
 echo "압축 해제 완료!"
 
 # 2. JAR 파일 찾기
-JAR_NAME=$(ls $REPOSITORY/build/libs/ | grep 'SNAPSHOT.jar' | tail -n 1)
-JAR_PATH=$REPOSITORY/build/libs/$JAR_NAME
+JAR_NAME=$(ls $REPOSITORY/app/build/libs/ | grep 'SNAPSHOT.jar' | tail -n 1)
+JAR_PATH=$REPOSITORY/app/build/libs/$JAR_NAME
 
-# 현재 실행 중인 프로세스 찾아서 종료
+# 3. 현재 실행 중인 프로세스 찾아서 종료
 CURRENT_PID=$(pgrep -f $APP_NAME)
 if [ -z $CURRENT_PID ]
 then
@@ -26,6 +26,6 @@ else
   sleep 5
 fi
 
-# 새로운 JAR 파일 실행
+# 4. 새로운 JAR 파일 실행
 echo "> Deploy - $JAR_PATH "
 nohup java -jar $JAR_PATH > /dev/null 2> /dev/null < /dev/null &
