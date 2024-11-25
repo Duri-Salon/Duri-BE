778c75684a343080c306df03fb9a1fcf0743f39f
delight-es <skfmf7878@gmail.com>
2024-11-25 15:08:55 +0900

    feat: Code Deploy 연결 및 EC2 자동 배포 (DURI-311)

---
+++ scripts/deploy.sh
@@ -0,0 +1,31 @@
+#!/usr/bin/env bash
+
+APP_NAME=duri
+
+# 배포 파일 경로 이동
+REPOSITORY=/home/ubuntu/duri
+cd $REPOSITORY
+
+# 압축 해제
+echo "압축 해제 시작..."
+unzip -o $REPOSITORY/duri-aws-build.zip -d $REPOSITORY/
+echo "압축 해제 완료!"
+
+# 2. JAR 파일 찾기
+JAR_NAME=$(ls $REPOSITORY/build/libs/ | grep 'SNAPSHOT.jar' | tail -n 1)
+JAR_PATH=$REPOSITORY/build/libs/$JAR_NAME
+
+# 현재 실행 중인 프로세스 찾아서 종료
+CURRENT_PID=$(pgrep -f $APP_NAME)
+if [ -z $CURRENT_PID ]
+then
+  echo "> 종료할 애플리케이션이 없습니다."
+else
+  echo "> kill -9 $CURRENT_PID"
+  kill -15 $CURRENT_PID
+  sleep 5
+fi
+
+# 새로운 JAR 파일 실행
+echo "> Deploy - $JAR_PATH "
+nohup java -jar $JAR_PATH > /dev/null 2> /dev/null < /dev/null &
