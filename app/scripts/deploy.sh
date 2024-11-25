d70ff088dbe91540c5b41cd4148f32c5780dd924
delight-es <skfmf7878@gmail.com>
2024-11-25 18:17:49 +0900

    bug: 압축 해제 제거 (DURI-311)

--- app/scripts/deploy.sh
+++ app/scripts/deploy.sh
@@ -2,18 +2,14 @@
 
 APP_NAME=app
 
-# 배포 파일 경로 이동
+# 1. 배포 파일 경로 이동
 REPOSITORY=/home/ubuntu/duri
 cd $REPOSITORY
-
-# 1. 압축 해제
-echo "압축 해제 시작..."
-unzip -o $REPOSITORY/duri-aws-build.zip -d $REPOSITORY/
-echo "압축 해제 완료!"
 
 # 2. JAR 파일 찾기
 JAR_NAME=$(ls $REPOSITORY/build/libs/ | grep 'SNAPSHOT.jar' | tail -n 1)
 JAR_PATH=$REPOSITORY/build/libs/$JAR_NAME
+echo "jar 파일 배포 성공"
 
 # 3. 현재 실행 중인 프로세스 찾아서 종료
 CURRENT_PID=$(pgrep -f $APP_NAME)
