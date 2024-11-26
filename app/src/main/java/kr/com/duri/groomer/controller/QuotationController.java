fe0d9e29127706a6d7ed7ef964347684dd45a9fa
taeyeon0319 <xodus1349@likelion.org>
2024-11-26 14:22:07 +0900

    Fix: url명 수정

--- app/src/main/java/kr/com/duri/groomer/controller/QuotationController.java
+++ app/src/main/java/kr/com/duri/groomer/controller/QuotationController.java
@@ -11,7 +11,7 @@ import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequiredArgsConstructor
-@RequestMapping("/api/v1/requests")
+@RequestMapping("/api/v1/groomer/requests")
 public class QuotationController {
 
     private final QuotationFacade quotationFacade;
