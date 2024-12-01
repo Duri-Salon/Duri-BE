0298c1067d302dbe13b0a06f5ee368c1e35aa6c5
delight-es <skfmf7878@gmail.com>
2024-12-02 00:11:42 +0900

    feat: Review, Pet 예외처리 추가 (DURI-286)

---
+++ app/src/main/java/kr/com/duri/user/exception/ReviewImageUploadException.java
@@ -0,0 +1,7 @@
+package kr.com.duri.user.exception;
+
+public class ReviewImageUploadException extends RuntimeException {
+    public ReviewImageUploadException(String message) {
+        super(message);
+    }
+}
