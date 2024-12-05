6701bcf3fae3f7717f4c073bc2aedb8e7e7557a9
delight-es <skfmf7878@gmail.com>
2024-12-05 14:11:18 +0900

    feat: 매장 리뷰 조회 관련 Domain 추가 (DURI-294)

---
+++ app/src/main/java/kr/com/duri/groomer/application/dto/response/ShopReviewResponse.java
@@ -0,0 +1,19 @@
+package kr.com.duri.groomer.application.dto.response;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class ShopReviewResponse {
+    private Long reviewId; // 리뷰 ID
+    private String reviewImageURL; // 리뷰 이미지 URL
+    private String comment; // 리뷰 내용
+    private int rating; // 별점
+    private Long userId; // 고객 ID
+    private String userImageURL; // 고객 이미지 URL
+}
--- app/src/main/java/kr/com/duri/user/repository/ReviewRepository.java
+++ app/src/main/java/kr/com/duri/user/repository/ReviewRepository.java
@@ -5,9 +5,29 @@ import java.util.List;
 import kr.com.duri.user.domain.entity.Review;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 public interface ReviewRepository extends JpaRepository<Review, Long> {
 
+    // 매장 ID 기준 리뷰 찾기
+    @Query(
+            value =
+                    """
+           SELECT rv FROM Review rv
+           JOIN rv.request rq
+           WHERE rq.shop.id = :shopId
+           """)
+    List<Review> findAllByShopId(@Param("shopId") Long shopId);
+
     // 반려견 ID 기준 리뷰 찾기
-    List<Review> findByPetId(Long petId);
+    @Query(
+            value =
+                    """
+            select rv from Review rv
+            join rv.request rq
+            join rq.quotation qr
+            where qr.pet.id = :petId
+            """)
+    List<Review> findByPetId(@Param("petId") Long petId);
 }
