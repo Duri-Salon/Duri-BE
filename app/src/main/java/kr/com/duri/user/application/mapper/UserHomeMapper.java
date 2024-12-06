3cef4ec36cd86190471d6f7ed0acc41e0c4c5c08
delight-es <skfmf7878@gmail.com>
2024-12-06 17:31:30 +0900

    feat: 사용자 다음 예약 조회 관련 Domain 추가 (DURI-275)

---
+++ app/src/main/java/kr/com/duri/user/application/mapper/UserHomeMapper.java
@@ -0,0 +1,49 @@
+package kr.com.duri.user.application.mapper;
+
+import java.time.format.DateTimeFormatter;
+
+import kr.com.duri.groomer.domain.entity.Quotation;
+import kr.com.duri.groomer.domain.entity.Shop;
+import kr.com.duri.groomer.domain.entity.ShopImage;
+import kr.com.duri.user.application.dto.response.RecentProcedureResponse;
+import kr.com.duri.user.domain.entity.Pet;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class UserHomeMapper {
+
+    // create Empty RecentProcedureResponse DTO
+    public RecentProcedureResponse createEmpty(Long petId, Integer lastSinceDay) {
+        return RecentProcedureResponse.builder().petId(petId).lastSinceDay(lastSinceDay).build();
+    }
+
+    // Entity to RecentProcedureResponse DTO
+    public RecentProcedureResponse toRecentProcedureResponse(
+            Quotation quotation,
+            Pet pet,
+            Shop shop,
+            ShopImage shopImage,
+            Integer lastSinceDay,
+            Integer reserveDday) {
+        DateTimeFormatter DayTimeFormatter = DateTimeFormatter.ofPattern("yyyy-MM-dd hh");
+        return RecentProcedureResponse.builder()
+                .petId(pet.getId())
+                .lastSinceDay(lastSinceDay)
+                .shopId(shop.getId())
+                .imageURL(
+                        shopImage.getShopImageUrl()) // != null ? shopImage.getShopImageUrl() : "")
+                .name(shop.getName()) // != null ? shop.getName() : "")
+                .address(shop.getAddress()) // != null ? shop.getAddress() : "")
+                .phone(shop.getPhone()) // != null ? shop.getPhone() : "")
+                .kakaoURL(shop.getKakaoTalk()) // != null ? shop.getKakaoTalk() : "")
+                .quotationId(quotation.getId())
+                .reserveDday(reserveDday)
+                .reservationDate(
+                        quotation.getStartDateTime() != null
+                                ? quotation.getStartDateTime().format(DayTimeFormatter)
+                                : "")
+                .price(quotation.getPrice()) // != null? quotation.getPrice() : "")
+                .build();
+    }
+}
