1c683fd5d0c354d810118e85e8cbcf4942681af0
taeyeon0319 <xodus1349@likelion.org>
2024-12-06 14:17:20 +0900

    fix: 견적요청서 상세정보 API 응답 정보 수정

--- app/src/main/java/kr/com/duri/user/application/dto/response/MenuDetailResponse.java
+++ app/src/main/java/kr/com/duri/user/application/dto/response/MenuDetailResponse.java
@@ -1,11 +1,11 @@
 package kr.com.duri.user.application.dto.response;
 
+import java.time.LocalDate;
+
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
-
-import java.time.LocalDate;
 
 @Getter
 @AllArgsConstructor
@@ -19,14 +19,14 @@ public class MenuDetailResponse {
     private String designCut; // 디자인 컷
     private String otherRequests; // 기타 요청 사항
     private LocalDate day; // 희망 날짜
-    private Boolean time9; //9시 희망 여부
-    private Boolean time10; //10시 희망 여부
-    private Boolean time11; //11시 희망 여부
-    private Boolean time12; //12시 희망 여부
-    private Boolean time13; //13시 희망 여부
-    private Boolean time14; //14시 희망 여부
-    private Boolean time15; //15시 희망 여부
+    private Boolean time9; // 9시 희망 여부
+    private Boolean time10; // 10시 희망 여부
+    private Boolean time11; // 11시 희망 여부
+    private Boolean time12; // 12시 희망 여부
+    private Boolean time13; // 13시 희망 여부
+    private Boolean time14; // 14시 희망 여부
+    private Boolean time15; // 15시 희망 여부
     private Boolean time16; // 16시 희망 여부
-    private Boolean time17; //17시 희망 여부
-    private Boolean time18; //18시 희망 여부
+    private Boolean time17; // 17시 희망 여부
+    private Boolean time18; // 18시 희망 여부
 }
