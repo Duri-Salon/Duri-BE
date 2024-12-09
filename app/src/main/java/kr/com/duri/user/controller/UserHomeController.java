bddb3410a3d5e2a87ae3aa7bd566b83070feae49
delight-es <skfmf7878@gmail.com>
2024-12-09 16:20:27 +0900

    feat: 고객 홈 - 매장 제안 Controller 추가 (DURI-329)

--- app/src/main/java/kr/com/duri/user/controller/UserHomeController.java
+++ app/src/main/java/kr/com/duri/user/controller/UserHomeController.java
@@ -1,8 +1,11 @@
 package kr.com.duri.user.controller;
 
+import java.util.List;
+
 import kr.com.duri.common.response.CommonResponseEntity;
 import kr.com.duri.user.application.dto.response.HomePetInfoResponse;
 import kr.com.duri.user.application.dto.response.RecentProcedureResponse;
+import kr.com.duri.user.application.dto.response.RecommendShopResponse;
 import kr.com.duri.user.application.dto.response.RegularShopResponse;
 import kr.com.duri.user.application.facade.UserHomeFacade;
 import lombok.RequiredArgsConstructor;
@@ -10,6 +13,7 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -39,5 +43,17 @@ public class UserHomeController {
     public CommonResponseEntity<HomePetInfoResponse> getPetInfo(@PathVariable Long userId) {
         HomePetInfoResponse homePetInfoResponse = userHomeFacade.getPetInfo(userId);
         return CommonResponseEntity.success(homePetInfoResponse);
+    }
+
+    // DURI-329 : 추천 매장 조회
+    @GetMapping("/recommend/{userId}")
+    public CommonResponseEntity<List<RecommendShopResponse>> getRecommendShops(
+            @PathVariable Long userId, @RequestParam Double lat, @RequestParam Double lon) {
+        // TODO : 매장 추천 조회 연결되는지 확인 후, 안되면 수도권 기준
+        lat = (lat != null) ? 37.510257428761 : lat;
+        lon = (lon != null) ? 127.04391561527 : lon;
+        List<RecommendShopResponse> recommendShopResponses =
+                userHomeFacade.getRecommendShops(userId, lat, lon);
+        return CommonResponseEntity.success(recommendShopResponses);
     }
 }
