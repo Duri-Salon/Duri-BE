0f981f741085e8d20bb225daf47326747e59d4cb
rlawotmd <kimjs2400@naver.com>
2024-12-11 20:11:28 +0900

    refactor: 고객 토큰 발급 리팩토링

--- app/src/main/java/kr/com/duri/user/application/facade/UserAuthFacade.java
+++ app/src/main/java/kr/com/duri/user/application/facade/UserAuthFacade.java
@@ -4,6 +4,7 @@ import kr.com.duri.user.application.dto.response.NewUserJwtResponse;
 import kr.com.duri.user.application.mapper.UserJwtMapper;
 import kr.com.duri.user.application.service.SiteUserService;
 import kr.com.duri.user.domain.entity.SiteUser;
+import kr.com.duri.user.exception.UserNotFoundException;
 import lombok.RequiredArgsConstructor;
 
 import org.springframework.stereotype.Component;
@@ -20,7 +21,7 @@ public class UserAuthFacade {
         SiteUser siteUser =
                 siteUserService
                         .findBySocialId(providerId)
-                        .orElseThrow(() -> new IllegalArgumentException("사용자를 찾을 수 없습니다."));
+                        .orElseThrow(() -> new UserNotFoundException("사용자를 찾을 수 없습니다."));
 
         String token = siteUserService.createNewUserJwt(siteUser);
 
